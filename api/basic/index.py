de630adf975dedfdc9aa01f712a648a2d2343c6c
Manfredi Bernardi <manfredibernardi@Manfredis-MacBook-Air.local>
2025-03-13 20:14:25 +0100

    Create absolute minimum Vercel serverless function

---
+++ api/basic/index.py
@@ -0,0 +1,8 @@
+def handler(request, context):
+    """
+    Basic Vercel Python serverless function.
+    """
+    return {
+        "statusCode": 200,
+        "body": "Hello from Vercel Python serverless function!"
+    } 
